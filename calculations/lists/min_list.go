a9bcf5c0e412d13dc3c90245b5da9fd04cb8e13d
Naumann05 <felix.naumann05@gmail.com>
2024-12-20 11:29:54 +0100

    aufgaben

--- calculations/lists/min_list.go
+++ calculations/lists/min_list.go
@@ -1,8 +1,21 @@
 package lists
 
+import (
+	"math"
+)
+
 // Erwartet eine Liste von Zahlen und gibt das Minimum zurück.
 // Wenn die Liste leer ist, wird 0 zurückgegeben.
 func MinList(nums []int) int {
 	// TODO
-	return 0
+	smallest := math.MaxInt
+	if len(nums) == 0 {
+		return 0
+	}
+	for _, num := range nums {
+		if num < smallest {
+			smallest = num
+		}
+	}
+	return smallest
 }
